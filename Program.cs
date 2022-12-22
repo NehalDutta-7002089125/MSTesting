25ed278c14f3d027634bd2117fefd28a9390689a
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2022-12-22 17:35:51 +0530

    Nehal

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Factor_TC
+{
+    public class Program
+    {
+        string mood;
+        
+
+        public Program(string mood)
+        {
+            this.mood = mood;
+            
+        }
+
+        public string Analyse()
+        {
+            if (mood.Contains("sad"))
+            {
+                return "i am sad";
+            }
+          
+            else
+            {
+                return "happy";
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Program a = new Program("sad");
+           
+
+            Console.WriteLine("enter mood");
+            string c= Console.ReadLine();
+            Console.WriteLine(a.Analyse());
+           
+
+
+        }
+    }
+}
