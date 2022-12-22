7ede8fdb035f92b7c4c03c0f7a02ed93d84b400c
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2022-12-22 16:03:34 +0530

    Nehal

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyse;
+
+namespace MoodAnalyse
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program a = new Program();
+          
+              string mood =a.Analyse("sad");
+           
+            Assert.AreEqual(mood,"sad");
+
+
+        }
+        
+
+    }
+}
