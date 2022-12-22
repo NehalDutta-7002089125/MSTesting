49af91e16c3f3cba774d651f6696550c6737e3f1
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2022-12-22 15:57:55 +0530

    Nehal

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MoodAnalyse
+{
+    public class Program
+    {
+        
+        public string Analyse(string mood)
+        {
+            if (mood.Contains("sad"))
+            {
+                return "sad";
+            }
+           
+            else
+            {
+                return "i happy";
+            }
+            
+        }
+    }
+    public class e
+    {
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            Console.WriteLine("enter mood");
+            string c = Console.ReadLine();
+            Console.WriteLine(a.Analyse(c));
+        }
+    }
+    
+}
