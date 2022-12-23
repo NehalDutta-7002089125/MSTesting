24d10db7222c3371899233810d295b104048666e
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2022-12-23 18:54:41 +0530

    Nehal

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TC_uc_2
+{
+    public class Program
+    {
+      
+            static void Main(string[] args)
+        {
+            Console.WriteLine("enter mood");
+            string c = Console.ReadLine();
+            
+            mood a = new mood(c);
+           
+          
+             Console.WriteLine(a.analyse());
+        }
+    }
+}
---
+++ mood.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TC_uc_2
+{
+    public class mood
+    {
+        string Message;
+        public mood(string message)
+        {
+            this.Message = message;
+        }
+        public string analyse()
+        {
+
+
+
+            if (Message.Contains("sad"))
+            {
+                return "sad";
+            }
+
+            else
+            {
+                return "happy";
+            }
+
+
+        }
+
+        //public string AnalyseMethodInvalidInput()
+        //{
+        //    try
+        //    {
+        //        if (Message.Contains("sad"))
+        //        {
+        //            return "sad";
+        //        }
+        //        else
+        //        {
+        //            return "happy";
+        //        }
+
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return "Happy";
+        //    }
+        //}
+    }
+}
